aa87fd4e196995638130ec61d9891f5c36baffe7
G13-12AMU <96543729+G13-12AMU@users.noreply.github.com>
2021-12-22 15:27:18 -0800

    Create ScannerFileReader.java

---
+++ ScannerFileReader.java
@@ -0,0 +1,21 @@
+package com.ScannerFileIO;
+import java.io.File;
+import java.util.Scanner;
+public class ScannerFileReader {
+
+    public static void main(String[] args) {
+        try {
+            System.out.print("Enter the file name with extension : ");
+            Scanner input = new Scanner(System.in);
+            File file = new File(input.nextLine());
+            input = new Scanner(file);
+            while (input.hasNextLine()) {
+                String line = input.nextLine();
+                System.out.println(line);
+            }
+            input.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
